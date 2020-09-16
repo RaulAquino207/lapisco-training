0c95c8ceef899b81cef1d66fd504cba29d085bc7
Raul Aquino <aquinoraul207@gmail.com>
2020-09-15 21:00:16 -0300

    showing the input image

--- topics/9/topic_9.py
+++ topics/9/topic_9.py
@@ -2,7 +2,8 @@
 Questão 9
 Abrir uma imagem colorida, transformar em tom de cinza, visualizar imagem de entrada.
 Criem uma matriz de forma estática com as mesmas dimensões da imagem de entrada (vejam nas propriedades da imagem no Windows),
-peguem cada um dos pixels da imagem e coloquem na matriz que criaram. Imprimam esta matriz em um arquivo de texto (*.txt) do mesmo modo que ela está alocada.
+peguem cada um dos pixels da imagem e coloquem na matriz que criaram.
+Imprimam esta matriz em um arquivo de texto (*.txt) do mesmo modo que ela está alocada.
 '''
 #library import
 import cv2 as cv
@@ -18,6 +19,9 @@ is only necessary in an image with 3 color channels (RGB), in the grayscale imag
 rows, cols = grayscale_img.shape
 #showing the image size
 print(rows,cols)
+#show the input image
+cv.imshow('Input grayscale image', grayscale_img)
+cv.waitKey(0)
 #saving the content of the image in the .txt file, using the with function which will do the close function when the block is closed
 #Save all pixels in a txt file
 with open('result.txt', 'w') as outfile:
